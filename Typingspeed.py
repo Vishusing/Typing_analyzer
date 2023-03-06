5929402b06a113c3ecb93dcf891867e17c3916bb
Vishal Singh <vs299901@gmail.com>
2023-03-06 20:21:58 +0530

    Add files via upload

---
+++ Typingspeed.py
@@ -0,0 +1,68 @@
+from time import time # to record the time 
+
+
+# now to calculate the accuracy of input prompt 
+
+def tperror(prompt):
+    global inwords 
+
+
+    words = prompt.split()
+    errors = 0 
+
+    for i in range(len(inwords)):
+        if i in (0, len(inwords)-1):
+            if inwords[i] == words[i]:
+                continue
+            else:
+                errors+=1 
+        else:
+            if inwords[i] == words[i]:
+                if(inwords[i+1] == words[i+1]) and (inwords[i-1] == words[i-1]):
+                    continue
+                else:
+                    errors += 1
+            else:
+                errors+=1
+    return errors 
+
+# now to calculate the speed of typing words per minute 
+def speed(inprompt, stime, etime):
+    global time 
+    global inwords 
+
+    inwords = inprompt.split()
+    twords = len(inwords)
+    speed = twords / time 
+
+    return speed 
+
+# calculate the total elapsed time 
+def elapsedtime(stime, etime):
+    time = etime - stime      # etime is the end time and stime is the start time  
+    return time 
+
+
+if __name__ == "__main__":
+    prompt = "Python is a high-level, interpreted programming language"
+# this is the paragraph which you have to type to check your speed.
+    print("Type this:-'",prompt)
+
+    # checking to input Enter basically it will see 
+    input("Press enter when you are ready to check your speed!!!!\n\n")
+
+
+    # recording time for input 
+    stime = time()
+    inprompt = input()
+    etime = time()
+
+    # collect all the info returned by the functions
+    time = round(elapsedtime(stime,etime),2)
+    speed = speed(inprompt,stime,etime)
+    errors = tperror(prompt)
+
+    #printing all the required data to see the result 
+    print('Total time elapsedL', time ,'seconds')
+    print('Your average typing speed was',speed,'words per minute (w/m)')
+    print('With the total of',errors,'errors')
